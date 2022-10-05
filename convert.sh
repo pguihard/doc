52f54d5cae6425e49e514578da14fed3f6a476e2
Phiippe Guihard <philippe.guihard@cegetel.net>
2022-10-05 14:38:55 +0200

    go ahead

--- convert.sh
+++ convert.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-bash -c 'cd /home/fifix/Workspace/Python/conversion; ./venv/bin/python docx2html.py'
+bash -c 'cd /home/fifix/Workspace/Python/conversion; ./venv/bin/python docx2html.py CA_1.1'
 read -p "Press any key to finish ..."

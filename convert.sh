d94167df1b60e334426e87cca2de3530fde8211b
Phiippe Guihard <philippe.guihard@cegetel.net>
2022-11-17 10:09:09 +0100

    17/11/2022 10:09:09

--- convert.sh
+++ convert.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/bash
-bash -c 'cd /home/fifix/Workspace/Python/conversion; ./venv/bin/python docx2html.py CA_1.1'
+# Argument is the name of the docx file without the .docx extension, e.g.: CA_1.1 or JP_2.1
+if [ $# -eq 0 ]
+  then
+    echo "No input arguments exist"
+    exit 1
+fi
+bash -c "cd /home/fifix/Workspace/Python/conversion; ./venv/bin/python docx2html.py $1"
 read -p "Press any key to finish ..."

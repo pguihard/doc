b797dca46d611d9b6f76168602cb04b5d620503e
Phiippe Guihard <philippe.guihard@cegetel.net>
2022-11-20 16:16:17 +0100

    clean

--- convert.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-# Argument is the name of the docx file without the .docx extension, e.g.: CA_1.1 or JP_2.1
-if [ $# -eq 0 ]
-  then
-    echo "No input arguments exist"
-    exit 1
-fi
-bash -c "cd /home/fifix/Workspace/Python/conversion; ./venv/bin/python docx2html.py $1"
-read -p "Press any key to finish ..."
